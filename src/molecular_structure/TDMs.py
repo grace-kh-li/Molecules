Completed CaNH2 structure and TMD calculation example notebook. Fixed the dumbest bug I've ever seen (forgot to type  - other.matrix in Operators __sub__ subtraction method!!!)

src/molecular_structure/TDMs.py
@@ -14,17 +14,17 @@ class DipoleOperator(Operator):
 
 
 
-# class DipoleOperator_STM(DipoleOperator):
-#     """ Electric dipole moment operator, for STM basis """
-#     def __init__(self, basis, dipole_mol, sigma_space, symmetry_group=None, irrep=None):
-#         """ STM basis, rotational transitions """
-#         matrix = np.zeros((len(basis), len(basis)), dtype=np.complex128)
-#         for sigma_mol in (-1, 0, 1):
-#             D = D_matrix_conj(basis, sigma_space, sigma_mol)
-#             for i, b in enumerate(basis):
-#                 for i1, b1 in enumerate(basis):
-#                     matrix[i,i1] += dipole_mol[1][sigma_mol] * D[i,i1]
-#         super().__init__(sigma_space, basis, matrix, symmetry_group, irrep)
+class DipoleOperator_STM(DipoleOperator):
+    """ Electric dipole moment operator, for STM basis """
+    def __init__(self, basis, dipole_mol, sigma_space, symmetry_group=None, irrep=None):
+        """ STM basis, rotational transitions """
+        matrix = np.zeros((len(basis), len(basis)), dtype=np.complex128)
+        for sigma_mol in (-1, 0, 1):
+            D = D_matrix_conj(basis, sigma_space, sigma_mol)
+            for i, b in enumerate(basis):
+                for i1, b1 in enumerate(basis):
+                    matrix[i,i1] += dipole_mol[1][sigma_mol] * D[i,i1]
+        super().__init__(sigma_space, basis, matrix, symmetry_group, irrep)
 
 
 class DipoleOperator_evr(DipoleOperator):

src/quantum_mechanics/Basis.py
@@ -105,7 +105,7 @@ class QuantumState:
         self.sorted = sorted
 
         for i, b in enumerate(self.defining_basis):
-            if np.abs(self.coeff[i]) > 1e-6:
+            if np.abs(self.coeff[i]) > 1e-5:
                 self.non_zero_basis.append(b)
                 self.non_zero_coeffs.append(coeff[i])
 

src/quantum_mechanics/Operator.py
@@ -9,12 +9,16 @@ class Operator:
         self.irrep = irrep
 
     def matrix_element(self, state1, state2):
-        return np.conj(state1.coeff) @ self.matrix @ state2.coeff
+        assert state1.defining_basis == self.basis == state2.defining_basis
+        return np.conj(state1.coeff.T) @ self.matrix @ state2.coeff
 
     def __getitem__(self, key):
         s1, s2 = key
         if isinstance(s1, int) and isinstance(s2, int):
             return self.matrix[s1, s2]
+        elif isinstance(s1, BasisVector) and isinstance(s2, BasisVector):
+            assert s1 in self.basis and s2 in self.basis
+            return self.matrix[self.basis.get_index(s1), self.basis.get_index(s2)]
         elif isinstance(s1, QuantumState) and isinstance(s2, QuantumState):
             return self.matrix_element(s1, s2)
         else:
@@ -39,7 +43,7 @@ class Operator:
 
     def __sub__(self, other):
         assert self.basis == other.basis
-        return Operator(self.basis, self.matrix)
+        return Operator(self.basis, self.matrix - other.matrix)
 
     def __mul__(self, other):
         if isinstance(other, Operator):

src/tools/SphericalTensors.py
@@ -54,7 +54,7 @@ class SphericalTensor:
             self.tensors = [T1]
             T1[0] = T[2]
             T1[-1] = (T[0] - T[1]*1j) * 1/np.sqrt(2)
-            T1[1] = (T[0] + T[1]*1j) *  (-1/np.sqrt(2) )
+            T1[1] = (T[0] + T[1]*1j) *  (-1/np.sqrt(2))
 
     def __getitem__(self, i):
         for t in self.tensors:
@@ -69,10 +69,32 @@ class SphericalTensor_prolate(SphericalTensor):
             xyz = np.array([T[1],T[2],T[0]])
             super().__init__(xyz, is_operator=is_operator, operator_basis=operator_basis)
         if T.shape == (3,3):
-            xyz = np.zeros((3,3))
+            if is_operator:
+                xyz = np.zeros((3,3),dtype=object)
+            else:
+                xyz = np.zeros((3,3), dtype=np.complex128)
             for i in range(3):
                 for j in range(3):
                     i1 = (i-1) % 3
+                    print(i, i1)
                     j1 = (j-1) % 3
                     xyz[i1,j1] = T[i,j]
             super().__init__(xyz, is_operator=is_operator, operator_basis=operator_basis)
+
+if __name__ == "__main__":
+    from src.molecular_structure.VibronicStates import VibronicState
+
+    X = VibronicState("X")
+    A = VibronicState("A")
+    B = VibronicState("B")
+    vibronic_basis = OrthogonalBasis([X, A, B], "Electronic")
+    vibronic_d_a = Operator(vibronic_basis, np.array([[0, 0, 0], [0, 0, 0], [0, 0, 0]]))
+    vibronic_d_b = Operator(vibronic_basis, np.array([[0, 1, 0], [1, 0, 0], [0, 0, 0]]))
+    vibronic_d_c = Operator(vibronic_basis, np.array([[0, 0, 1], [0, 0, 0], [1, 0, 0]]))
+
+    # the transition dipole moment in molecule frame as a spherical tensor
+    vibronic_d = SphericalTensor_prolate(np.array([vibronic_d_a, vibronic_d_b, vibronic_d_c]), is_operator=True,
+                                         operator_basis=vibronic_basis)
+
+    print(vibronic_d[1][1])
+    print(vibronic_d[1][-1])